d43484b51df1d8c79a580620ca813e5c6b61e707
Sümeyye Aslan <sultan@syn-172-100-100-074.res.spectrum.com>
2024-09-16 02:23:22 +0300

    r

--- frontend/src/sections/infoCardSlider-section.tsx
+++ frontend/src/sections/infoCardSlider-section.tsx
@@ -25,7 +25,7 @@ const InfoCardSliderSection: React.FC<InfoCardSliderSectionProps> = ({
   return (
     <div
       ref={ref}
-      className={`relative max-w-[1600px] mx-auto h-auto transition-all duration-1000 ease-in-out ${
+      className={`relative max-w-[1600px] mx-auto h-[330px] transition-all duration-1000 ease-in-out ${
         inView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
       }`}
     >
